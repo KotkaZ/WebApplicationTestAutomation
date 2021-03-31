6aafe620b26831aa071b9b3885115290f04f389b
Allan Loo <loo_allan@yahoo.com>
2021-03-31 18:58:48 +0300

    AdminTest testid

--- src/test/java/AdminTest.java
+++ src/test/java/AdminTest.java
@@ -1,14 +1,127 @@
+
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.junit.Assert.*;
 
 public class AdminTest extends TestHelper  {
-    private String username = "admin";
-    private String password = "admin";
 
+    @Before
+    public void before() {
+        register();
+    }
 
     @Test
     public void registerAccount() {
+
+        boolean isPresent = isElementPresent(By.xpath("//a[contains(text(), 'Delete')]"));
+        assertTrue(isPresent);
+
         driver.get(baseUrlAdmin);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.linkText("Delete")).click();
 
     }
+
+    @Test
+    public void deleteAccount() {
+
+        driver.get(baseUrlAdmin);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.linkText("Delete")).click();
+        String deleteString = driver.findElement(By.id("notice")).getText();
+        assertEquals("User was successfully deleted.", deleteString);
+    }
+
+    @Test
+    public void addProduct() {
+        driver.findElement(By.linkText("New product")).click();
+
+        driver.findElement(By.id("product_title")).sendKeys("testItem");
+        driver.findElement(By.id("product_description")).sendKeys("testItem desc");
+        Select type = new Select(driver.findElement(By.id("product_prod_type")));
+        type.selectByValue("Books");
+        driver.findElement(By.id("product_price")).sendKeys("20");
+
+        driver.findElement(By.name("commit")).click();
+        driver.findElement(By.id("testItem")).findElement(By.linkText("Delete")).click();
+
+        assertFalse(driver.findElements(By.id("testItem")).size() > 0);
+
+        driver.get(baseUrlAdmin);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.linkText("Delete")).click();
+    }
+
+    @Test
+    public void editProduct() {
+        driver.findElement(By.linkText("New product")).click();
+
+        driver.findElement(By.id("product_title")).sendKeys("testItem");
+        driver.findElement(By.id("product_description")).sendKeys("testItem desc");
+        Select type = new Select(driver.findElement(By.id("product_prod_type")));
+        type.selectByValue("Books");
+        driver.findElement(By.id("product_price")).sendKeys("20");
+        driver.findElement(By.name("commit")).click();
+
+        driver.findElement(By.id("testItem")).findElement(By.linkText("Edit")).click();
+
+        driver.findElement(By.id("product_title")).sendKeys(Keys.chord(Keys.CONTROL, "a"), "testItemEdited");
+        driver.findElement(By.name("commit")).click();
+        driver.findElement(By.linkText("Back")).click();
+
+        assertTrue(driver.findElements(By.id("testItemEdited")).size() > 0);
+
+        driver.findElement(By.id("testItemEdited")).findElement(By.linkText("Delete")).click();
+
+        driver.get(baseUrlAdmin);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.linkText("Delete")).click();
+    }
+
+    @Test
+    public void deleteProduct() {
+        driver.findElement(By.linkText("New product")).click();
+
+        driver.findElement(By.id("product_title")).sendKeys("testItem");
+        driver.findElement(By.id("product_description")).sendKeys("testItem desc");
+        Select type = new Select(driver.findElement(By.id("product_prod_type")));
+        type.selectByValue("Books");
+        driver.findElement(By.id("product_price")).sendKeys("20");
+        driver.findElement(By.name("commit")).click();
+
+        driver.findElement(By.id("testItem")).findElement(By.linkText("Delete")).click();
+
+        assertFalse(driver.findElements(By.id("testItem")).size() > 0);
+
+        driver.get(baseUrlAdmin);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.linkText("Delete")).click();
+    }
+
+
 }
--- src/test/java/TestHelper.java
+++ src/test/java/TestHelper.java
@@ -7,9 +7,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 public class TestHelper {
 
@@ -81,6 +84,17 @@ public class TestHelper {
 
         waitForElementById("Admin");
     }
+
+    void register(){
+        driver.get(baseUrlAdmin);
+        driver.findElement(By.linkText("Register")).click();
+        driver.findElement(By.id("user_name")).sendKeys("admin2");
+        driver.findElement(By.id("user_password")).sendKeys("admin2");
+        driver.findElement(By.id("user_password_confirmation")).sendKeys("admin2");
+        driver.findElement(By.name("commit")).click();
+    }
+
+
 
     @After
     public void tearDown(){
